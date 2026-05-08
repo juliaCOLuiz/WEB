868ad41a11313a4ab8afebef2613d7efd6a2bcd7
Your Name <you@example.com>
2026-05-08 11:53:56 -0300

    JavaScript

---
+++ javaScript/javascript.js
@@ -0,0 +1,2 @@
+alert("Alerta de arquivo externo");
+document.querySelector("button").innerHTML="Amanhã vou estudar cedinho";
---
+++ javaScript/variaveis.js
@@ -0,0 +1,20 @@
+let a=10
+console.log(a)
+console.log(typeof(a))
+a="dez"
+console.log(a)
+console.log(typeof(a))
+a=true
+console.log(a)
+console.log(typeof(a))
+a=5
+console.log(a+"0")
+console.log("0"+a)
+//const pi=2/ nao pode
+let contador = 0
+contador++
+console.log(contador)
+
+for (contador=0;contador<10;contador++){
+    console.log(contador)
+}
